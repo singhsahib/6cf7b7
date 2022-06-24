0d45c1b9d3e4b4bead1634202d11b3af4f738b8a
singhsahib <ssubhi.24@gmail.com>
2022-06-24 06:11:15 -0700

    updated version of the pagination web app.

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,22 +1,62 @@
 import BlogPost from "./BlogPost";
 import Pagination from "./Pagination";
-import React from "react";
+import React, { useState } from "react";
 import blogs from "../data/blogs.json";
+import { element } from "prop-types";
 
 const PAGE_SIZES = [15, 25, 50, 100];
 
 function BlogList() {
-  const currentPaginationData = blogs.posts.slice(0, 15);
+  const [pageSize, setPageSize] = useState(
+    blogs.posts.length < PAGE_SIZES[0] ? blogs.posts.length : PAGE_SIZES[0]
+  );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [start, setStart] = useState(0 * currentPage);
+  const [end, setEnd] = useState(pageSize);
+  const currentPaginationData = blogs.posts.slice(start, end);
 
-  const updateRowsPerPage = () => {};
-  const updatePage = () => {};
+  /**
+   * This function basically does some sort of transformation using the old currentPage number and new currentPage 
+    number so when you change from 100 per page to 15 the currentPage should also change and so does the array.
+   * @param value is the number of items required by the user per page. 
+   */
+  const updateRowsPerPage = (value) => {
+    let pageSizeIndex = PAGE_SIZES.findIndex((element) => element == value);
+
+    if (pageSizeIndex > -1) {
+      let oldLen = Math.ceil(blogs.posts.length / pageSize);
+      let newLen = Math.ceil(blogs.posts.length / PAGE_SIZES[pageSizeIndex]);
+      let oldCurrentPage = currentPage;
+      let newCurrentPage = Math.ceil(newLen * (oldCurrentPage / oldLen));
+
+      setEnd(newCurrentPage * PAGE_SIZES[pageSizeIndex]);
+      setStart((newCurrentPage - 1) * PAGE_SIZES[pageSizeIndex]);
+      setCurrentPage(newCurrentPage);
+      setPageSize(PAGE_SIZES[pageSizeIndex]);
+    } else {
+      setPageSize(PAGE_SIZES[0]);
+      setCurrentPage(1);
+      setStart(0);
+      setEnd(PAGE_SIZES[0]);
+    }
+  };
+
+  const updatePage = (pageNumber) => {
+    let len = Math.ceil(blogs.posts.length / pageSize);
+    if (!(pageNumber <= 0 || pageNumber > len)) {
+      setEnd(pageNumber * pageSize);
+      setStart((pageNumber - 1) * pageSize);
+
+      setCurrentPage(pageNumber);
+    }
+  };
 
   return (
     <div>
       <Pagination
-        currentPage={1}
+        currentPage={currentPage}
         totalCount={blogs.posts.length}
-        pageSize={15}
+        pageSize={pageSize}
         pageSizeOptions={PAGE_SIZES}
         onPageChange={updatePage}
         onPageSizeOptionChange={updateRowsPerPage}
--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,6 +1,6 @@
 export const DOTS = "...";
 
-function usePagination() {
+function usePagination({ currentPage, totalCount, pageSize }) {
   /*
     Rewrite the logic here to map out the pagination to be displayed
 
@@ -8,7 +8,28 @@ function usePagination() {
     Please replace this comment here with a description of this hook.
     
   */
-  return [1, 2, 3, DOTS, 5];
+
+  let len = Math.ceil(totalCount / pageSize);
+
+  if (totalCount <= pageSize) {
+    return [1, DOTS];
+  } else {
+    if (currentPage == 1 || currentPage == 2) {
+      return [1, 2, 3, DOTS, len];
+    } else if (currentPage == len || currentPage == len - 1) {
+      return [1, DOTS, len - 2, len - 1, len];
+    } else {
+      return [
+        1,
+        DOTS,
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        DOTS,
+        len,
+      ];
+    }
+  }
 }
 
 export default usePagination;
